f1f21e6f3c08b5c449a3b3beec5d0180d0b65413
Victor Leach <victor96@mac.com>
2016-04-22 19:04:57 +0100

    Small tweak.

--- gulpfile.js
+++ gulpfile.js
@@ -27,10 +27,10 @@ gulp.task("default", gulp.series("dev"));
  
 //! Setup
 gulp.task("setup", gulp.series(
-	gulp.parallel("stop")/*,
+	gulp.parallel("stop"),
 	gulp.parallel("setup.dependant", "setup.typings", "setup.docker", "setup.certs"),
 	gulp.parallel("build.semantic"),
-	gulp.parallel("database.reset")*/
+	//gulp.parallel("database.reset")
 ));
  
 //! Development
