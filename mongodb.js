b0e262eef77c396c5275c235d7734767b8ce3b51
Victor Leach <victor96@mac.com>
2016-04-23 12:37:00 +0100

    Increased wait time for MongoDB server to start.

--- mongodb.js
+++ mongodb.js
@@ -10,7 +10,7 @@ rs.initiate({
 
 //Wait for replica set to finish instantiating
 while (rs.status().startupStatus || (rs.status().hasOwnProperty("myState") && rs.status().myState != 1)){
-	sleep(1500);
+	sleep(2000);
 };
 
 //Create administrator account
