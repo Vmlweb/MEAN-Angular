aeb45bc8650256f3035e4cc2469973029088b517
Victor Leach <victor96@mac.com>
2017-01-23 23:01:05 +0000

    Removed zip dependancy from dist task.

--- tasks/dist.js
+++ tasks/dist.js
@@ -2,6 +2,7 @@
 const gulp = require('gulp')
 const shell = require('gulp-shell')
 const del = require('del')
+const docker = require('dockerode')()
 
 //Includes
 const config = require('../config.js')
@@ -9,6 +10,11 @@ const config = require('../config.js')
 /*! Tasks 
 - dist.clean
 - dist.build
+
+- dist.build
+- dist.build.docker
+- dist.build.zip
+- dist.build.clean
 
 - dist.copy
 - dist.copy.certs
@@ -22,17 +28,36 @@ gulp.task('dist.clean', function(){
 	return del('dist/**/*')
 })
 
-//Build and compress docker image for app
-gulp.task('dist.build', shell.task([
-	'docker build -t ' + config.name + ' $PWD',
-	'docker save ' + config.name + ' > ' + config.name + '.tar',
-	'zip ' + config.name + '.zip ' + config.name + '.tar',
-	'rm -r ' + config.name + '.tar',
-	'chmod +x server.sh'
-],{
-	verbose: true,
+//! Build
+gulp.task('dist.build', gulp.series(
+	'dist.build.docker',
+	'dist.build.zip',
+	'dist.build.clean'
+))
+
+//Generate docker image
+gulp.task('dist.build.docker', function(){
+	docker.buildImage(config.name + '.tar', {
+		t: config.name + '_app'
+	}, function (err, response){
+		done(err)
+	})
+}, {
 	cwd: 'dist'
-}))
+})
+
+//Compress docker image to zip
+gulp.task('dist.build.zip', function(){
+	return gulp.src(config.name + '.tar')
+		.pipe(gulp.dest('./'))
+}, {
+	cwd: 'dist'
+})
+
+//Remove non compressed docker image
+gulp.task('dist.build.clean', function(){
+	return del('dist/' + config.name + '.tar')
+})
 
 //! Copy
 gulp.task('dist.copy', gulp.parallel(
