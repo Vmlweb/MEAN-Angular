1bb889dcb96e62bfa7f5126f9097113e76c9be79
Victor Leach <victor96@mac.com>
2017-04-21 12:00:55 +0100

    Minor changes to file structure in server tests.

---
+++ server/tests/collection.ts
@@ -0,0 +1,58 @@
+//Modules
+import { Model } from 'mongoose'
+
+//Includes
+import { log, database } from 'app'
+import { CacheMonitor } from 'logic'
+
+export class Collection{
+	
+	modified = false
+	
+	constructor(public name: string, public model: Model<any>, public data: Object[]){
+		
+		//Subscribe to mongoose model change hooks and mark collection as modified
+		model.schema.pre('save', (next) => {
+			this.modified = true
+			next()
+		})
+		model.schema.pre('remove', (next) => {
+			this.modified = true
+			next()
+		})
+		model.schema.pre('update', (next) => {
+			this.modified = true
+			next()
+		})
+		
+		//Overide global mongoose model change hooks and mark collection as modified 
+		for (const hook of [ 'findByIdAndUpdate', 'findByIdAndRemove', 'findOneAndUpdate', 'findOneAndRemove', 'update', 'remove', 'create' ]){
+			(model as any)['___' + hook] = model[hook]
+			model[hook] = (...args) => {
+				this.modified = true
+				return (model as any)['___' + hook](...args)
+			}
+		}
+	}
+	
+	async reset(){
+		
+		//Drop collection contents silently
+		try{ 
+			await database.db.dropCollection(this.name)
+		}catch(err){}
+		
+		log.info('Clearing and populating ' + this.name + ' with test data')
+		
+		//Populate collection with test data and without validation
+		await this.model.insertMany(this.data)
+		
+		//Reset state
+		if (CacheMonitor.instance.caches.hasOwnProperty(this.name)){
+			await CacheMonitor.instance.caches[this.name].reload()
+		}
+		
+		//Reset modified flag
+		this.modified = false
+	}
+}
